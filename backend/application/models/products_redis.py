31002fd265071fef320f79d7283223b6346748c8
Dawid Nowotny <dawidnowotny@o2.pl>
2023-06-09 15:47:53 +0200

    Added cart handling

--- backend/application/models/products_redis.py
+++ backend/application/models/products_redis.py
@@ -1,11 +1,12 @@
 class ProductRedis:
-    def __init__(self, product, size):
+    def __init__(self, product, size, amount=1):
         self.product = product
         self.size = size
+        self.amount = amount
 
     def to_json(self):
-        return {'product': self.product, 'size': self.size}
+        return {'product': self.product, 'size': self.size, 'amount': self.amount}
 
     @classmethod
     def from_json(cls, json_data):
-        return cls(json_data['product'], json_data['size'])
+        return cls(json_data['product'], json_data['size'], json_data['amount'])
--- backend/run.py
+++ backend/run.py
@@ -8,14 +8,19 @@ from application.controllers.register import register_page
 
 from application.controllers.main_page import main_page_products
 from application.controllers.product_detail import product_details
-from application.controllers.handle_cart import add_to_cart
+from application.controllers.handle_cart import add_to_cart, get_cart, delete_from_cart, increase_product_amount
 
 app.register_blueprint(login_page)
 app.register_blueprint(google_login_page)
 app.register_blueprint(register_page)
+
 app.register_blueprint(main_page_products)
 app.register_blueprint(product_details)
+
 app.register_blueprint(add_to_cart)
+app.register_blueprint(get_cart)
+app.register_blueprint(delete_from_cart)
+app.register_blueprint(increase_product_amount)
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000, host='0.0.0.0')
