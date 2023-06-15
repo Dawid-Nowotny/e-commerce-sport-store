da0dad10c645be769a47e653df5f38afa1ff0ba5
Jakub Oselka <kubaoselka001@gmail.com>
2023-06-16 00:51:50 +0200

    Updated product details

--- frontend/src/app/product-details/product-details.component.ts
+++ frontend/src/app/product-details/product-details.component.ts
@@ -10,43 +10,85 @@ import { Sizes } from '../sizes/sizes';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+  isLoading: boolean = false;
   productId: string = '';
   item: Item[] = [];
   sizes: string[] = [];
   size: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
 
   constructor(private route: ActivatedRoute, private serverService: ServerService) {}
  
   ngOnInit() {
+    this.isLoading = true;
     this.route.url.subscribe(urlSegments => {
       this.productId = urlSegments[urlSegments.length - 1].toString();
-    
+  
       this.serverService.getDetails(this.productId).subscribe(response => {
         this.item = [response.items];
-      
-      if(this.item[0].category == "Buty")
-        this.sizes = Sizes.boots;
-      else if(this.item[0].category == "Ubranie")
-        this.sizes = Sizes.clothes;
-      else if(this.item[0].category == "Piłka")
-        this.sizes = Sizes.balls;
-      this.size = this.sizes[0];
+        if (this.item[0].category == "Buty")
+          this.sizes = Sizes.boots;
+        else if (this.item[0].category == "Ubranie")
+          this.sizes = Sizes.clothes;
+        else if (this.item[0].category == "Piłka")
+          this.sizes = Sizes.balls;
+  
+        if (this.isAvailable(this.size)) {
+          this.size = this.size; 
+        } else {
+          this.size = ''; 
+        }
+        
+        this.isLoading = false;
       });
     });
+  }
+
+  isAvailable(size: string): boolean {
+    if (this.item[0].sizes_and_amounts[this.parseSize(size)]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  parseSize(size: string): number {
+    const sizeMap: { [key: string]: number } = {
+      XS: 1,
+      S: 2,
+      M: 3,
+      L: 4,
+      XL: 5,
+      XXL: 6,
+    };
+
+    const numericValue = parseInt(size, 10);
+    if (!isNaN(numericValue)) {
+      return numericValue;
+    }
+  
+    return sizeMap[size as keyof typeof sizeMap] || 0;
   }
   
   onSubmit() {
     const data = {
       userId: localStorage.getItem("user_id"),
       productId: this.productId,
-      size: this.size
+      size: this.size.toString()
     }
 
-    this.serverService.addToCart(data).subscribe(
-      (response: any) => {
-        console.log('Odpowiedź serwera:', response);
-      }
-    );
+    if(data.size) {
+      this.serverService.addToCart(data).subscribe(
+        (response: any) => {
+          this.successMessage = response.message;
+          this.errorMessage = "";
+        }
+      );
+    } else {
+      this.successMessage = "";
+      this.errorMessage = "Musisz wybrać rozmiar!";
+    }
   }
 }
 
