d481b4e01ee20d7d25c1fb00b4526b490f37de33
Jakub Oselka <kubaoselka001@gmail.com>
2023-06-19 16:23:13 +0200

    Updated edit product component

--- frontend/src/app/edit-product/edit-product.component.ts
+++ frontend/src/app/edit-product/edit-product.component.ts
@@ -9,47 +9,65 @@ import { Item } from '../item/item';
   styleUrls: ['./edit-product.component.css']
 })
 export class EditProductComponent {
-  isAdmin: boolean = false;
+  isLogged: boolean = false;
+  admin: boolean = false;
   productId: string = ''
   items: Item[] = [];
   categories: any = '';
   brands: any = '';
-  
   name: string = '';
   brand_id: string = '';
   category_id: string = '';
   price: string = '';
   description: string = '';
   prod_images: File[] = [];
+  errorMessage: string = '';
+  successMessage: string = '';
 
   constructor(private route: ActivatedRoute, private serverService: ServerService) { }
 
   ngOnInit() {
-    this.serverService.isAdmin().subscribe(
-      (response: any) => {
-        if(response.isAdmin == true) {
-          this.isAdmin = true;
-          this.route.url.subscribe(urlSegments => {
-            this.productId = urlSegments[urlSegments.length - 1].toString();
-          
-            this.serverService.getDetailsForEdit(this.productId).subscribe(response => {
-              console.log(response.items.brand_id);
-              this.items = [response.items];
-              this.name = this.items[0].name;
-              this.brand_id = response.items.brand_id;
-              this.category_id = response.items.category_id;
-              this.price = this.items[0].price.toString();
-              this.description = this.items[0].description;
-            });
-          });
-          this.serverService.getBrandsAndCategories().subscribe(response => {
-            this.categories = response.categories;
-            this.brands = response.brands;
-            console.log(this.brands);
-          });
-        }
-      }
-    );
+    
+    if(this.serverService.admin == true) {
+      this.route.url.subscribe(urlSegments => {
+        this.productId = urlSegments[urlSegments.length - 1].toString();
+      
+        this.serverService.getDetailsForEdit(this.productId).subscribe(response => {
+          console.log(response.items.brand_id);
+          this.items = [response.items];
+          this.name = this.items[0].name;
+          this.brand_id = response.items.brand_id;
+          this.category_id = response.items.category_id;
+          this.price = this.items[0].price.toString();
+          this.description = this.items[0].description;
+        });
+      });
+      this.serverService.getBrandsAndCategories().subscribe(response => {
+        this.categories = response.categories;
+        this.brands = response.brands;
+        console.log(this.brands);
+      });
+    }
+  }
+  
+  ngAfterViewInit(): void {
+    this.isLogged = this.serverService.isLogged;
+    this.admin = this.serverService.admin;
+  }
+
+  onKeyPress(event: KeyboardEvent) {
+    const allowedKeys = ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight'];
+    const isDigit = /[0-9]/.test(event.key);
+    const isDot = event.key === '.' || event.key === 'Decimal';
+    const inputValue = (event.target as HTMLInputElement).value;
+  
+    if (!isDigit && !allowedKeys.includes(event.key) && !isDot) {
+      event.preventDefault();
+    }
+  
+    if (isDot && inputValue.includes('.')) {
+      event.preventDefault();
+    }
   }
 
   onSubmit(): void {
