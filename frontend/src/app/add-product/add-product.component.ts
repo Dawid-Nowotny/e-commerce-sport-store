93f71d5a72eb64da39c5a910ab05574d1be9ad5a
Jakub Oselka <kubaoselka001@gmail.com>
2023-06-19 15:04:10 +0200

    Updated add product component

--- frontend/src/app/add-product/add-product.component.ts
+++ frontend/src/app/add-product/add-product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ServerService } from '../server.service';
 
@@ -8,7 +8,8 @@ import { ServerService } from '../server.service';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
-  isAdmin: boolean = false;
+  isLogged: boolean = false;
+  admin: boolean = false;
   id: string = '';
   brand_id: string = '';
   category_id: number = 0;
@@ -18,48 +19,65 @@ export class AddProductComponent {
   prod_images: File[] = [];
   categories: any = '';
   brands: any = '';
+  errorMessage: string = '';
+  successMessage: string = '';
   
-  constructor(private titleService: Title, private serverService: ServerService) { }
+  constructor(private serverService: ServerService) { }
 
   ngOnInit() {
-    this.serverService.isAdmin().subscribe(
-      (response: any) => {
-        if(response.isAdmin == true) {
-          this.isAdmin = true;
-          this.titleService.setTitle('Dodaj produkt - AWAZONsport');
-          this.serverService.getBrandsAndCategories().subscribe(response => {
-            this.categories = response.categories;
-            this.brands = response.brands;
-            this.category_id = this.categories[0].id;
-            this.brand_id = this.brands[0].id;
-          });
-        }
-      }
-    );
+    this.serverService.getBrandsAndCategories().subscribe(response => {
+      this.categories = response.categories;
+      this.brands = response.brands;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.isLogged = this.serverService.isLogged;
+    this.admin = this.serverService.admin;
   }
 
   onFileChange(event: any) {
     this.prod_images = event.target.files;
   }
 
-  onSubmit(): void {
-    const formData = new FormData();
-    
-    formData.append('id', this.id.toString());
-    formData.append('brand_id', this.brand_id.toString());
-    formData.append('category_id', this.category_id.toString());
-    formData.append('description', this.description);
-    formData.append('name', this.name);
-    formData.append('price', this.price);
+  onKeyPress(event: KeyboardEvent) {
+    const allowedKeys = ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight'];
+    const isDigit = /[0-9]/.test(event.key);
+    const isDot = event.key === '.' || event.key === 'Decimal';
+    const inputValue = (event.target as HTMLInputElement).value;
   
-    for (let i = 0; i < this.prod_images.length; i++) {
-      formData.append('images[]', this.prod_images[i]);
+    if (!isDigit && !allowedKeys.includes(event.key) && !isDot) {
+      event.preventDefault();
     }
+  
+    if (isDot && inputValue.includes('.')) {
+      event.preventDefault();
+    }
+  }
+
+  onSubmit(): void {
+    if (this.name === '' || this.brand_id.toString() === null || this.category_id.toString() === null || this.description === '' || this.price === '' || this.prod_images === null) {
+      this.errorMessage = 'Wszystkie pola muszą być uzupełnione!';
+    } else {
+      const formData = new FormData();
     
-    this.serverService.addProduct(formData).subscribe(
-      (response: any) => {
-        console.log('Odpowiedź serwera:', response);
+      formData.append('id', this.id.toString());
+      formData.append('brand_id', this.brand_id.toString());
+      formData.append('category_id', this.category_id.toString());
+      formData.append('description', this.description);
+      formData.append('name', this.name);
+      formData.append('price', this.price);
+    
+      for (let i = 0; i < this.prod_images.length; i++) {
+        formData.append('images[]', this.prod_images[i]);
       }
-    );
+      
+      this.serverService.addProduct(formData).subscribe(
+        (response: any) => {
+          this.errorMessage = '';
+          this.successMessage = 'Produkt został dodany!';
+        }
+      );
+    }
   }
 }
